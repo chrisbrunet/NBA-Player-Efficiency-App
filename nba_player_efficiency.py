0cbb8a2b3d3a64bde67091c3309aa5a239d8ab11
chrisbrunet <132419309+chrisbrunet@users.noreply.github.com>
2023-06-06 12:39:28 -0600

    implemented csv import and data merge

--- nba_player_efficiency.py
+++ nba_player_efficiency.py
@@ -11,13 +11,22 @@
     # docstring on functions
     # names and group numbers included on all submitted files
     # proper spacing and naming conventions throughout
+import pandas as pd
 
 # import and merge data
-    # year, player names and teams as indices (index by team then player name)
-    # min 10 columns & 200 rows
-    # store as multi-indexted DataFrame
-    # delete all duplicated columns/rows
+    # DONE - year, player names and teams as indices (index by team then player name)
+    # DONE - min 10 columns & 200 rows
+    # DONE - store as multi-indexted DataFrame
+    # DONE - delete all duplicated columns/rows
     # data is sorted according to indices
+
+bios_data = pd.read_csv("data/bios.csv", index_col=[0,1,2], usecols=[0,1,2,4,5,6,7,8,9,10])
+speed_distance_data = pd.read_csv("data/speed_distance.csv", index_col=[0,1,2], usecols=[0,1,2,7,8,9,10,11,12,13])
+scoring_data = pd.read_csv("data/scoring_data.csv", index_col=[0,1,2])
+
+full_merge = pd.merge(pd.merge(bios_data, speed_distance_data, on=['YEAR', 'PLAYER', 'TEAM']), scoring_data, on=['YEAR', 'PLAYER', 'TEAM'])
+
+useful_data = full_merge[['WEIGHT','AGE', 'MIN', 'AVG SPEED', 'DIST. FEET', 'GP', 'PTS', 'FGA', 'W']].copy()
 
 # data manipulation (calculating player efficiency statistics)
     # program solution uses the describe method to print aggregate stats for the entire dataset
